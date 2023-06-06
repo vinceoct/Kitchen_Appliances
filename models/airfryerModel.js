8217b0d00713bd43714d4baa47b1c262519d9b27
Ron <ron_rose@me.com>
2023-06-06 14:55:26 -0400

    added to airfryerModel

--- models/airfryerModel.js
+++ models/airfryerModel.js
@@ -4,6 +4,12 @@ const db = require(`../db`)
 
 const AirFryer = new Schema(
     {
+        name: { type: String, required: true },
+        brand: { type: String, required: true },
+        dimensions: { type: String, required: true },
+        capacity: { type: String, required: true },
+        material: { type: String, required: true },    
+        image: { type: String, required: true }
 
     }
     
